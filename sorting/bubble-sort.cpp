f5dc5115aeb08e5b22082dc021659a915d8afcdc
Vivek <vivek.prajapati16072006@gmail.com>
2025-12-08 21:51:34 +0530

    day .\basic\ maths\ done.

--- sorting/bubble-sort.cpp
+++ sorting/bubble-sort.cpp
@@ -12,15 +12,38 @@ void bubble_sort(int arr[], int n)
             if (arr[j] > arr[i])
             {
                 swap(arr[j], arr[i]);
-                didSwap = 1
+                didSwap = 1;
             }
         }
-        if (swap == 0)
+        if (didSwap == 0)
             break;
     }
 }
 
-int main()
+void bubble_sort_rev(int arr[], int n)
+{
+    bool didSwap = false;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                swap(arr[j], arr[j + 1]);
+                didSwap = true;
+            }
+        }
+
+        if (!didSwap)
+        {
+            return;
+        }
+    }
+}
+
+void bubble_sort_recursion(vector<int> &arr)
+
+    int main()
 {
     // Your code here
     int n;
@@ -32,7 +55,7 @@ int main()
         cin >> arr[i];
     }
 
-    bubble_sort(arr, n);
+    bubble_sort_rev(arr, n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
--- sorting/insertion-sort.cpp
+++ sorting/insertion-sort.cpp
@@ -3,19 +3,34 @@ using namespace std;
 
 void insertion_sort(int arr[], int n)
 {
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i <= n - 1; i++)
     {
-        for (int j = i; j <= n - 1; j++)
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
         {
-            if (arr[j] > arr[j - 1])
-            {
-            }
+            swap(arr[j], arr[j - 1]);
+            j--;
         }
     }
 }
 
 int main()
 {
+    int n;
+    cout << "lenght of array :";
+    cin >> n;
+    int arr[n];
+    cout << arr[-1];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
